c037ff720381e71c94ba896e879351fba8893fe7
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-03-06 10:16:27 +0200

    Initial draft

--- src/LogNumbers.jl
+++ src/LogNumbers.jl
@@ -1,6 +1,26 @@
-__precompile__()
+import Base: show
+
 module LogNumbers
 
-# Package code goes here.
+mutable struct LogNumber{F<:AbstractFloat} <: AbstractFloat
+    log::F
+
+    LogNumber{T}() where T<:AbstractFloat = new{T}()
+    LogNumber{T}(x::T) where T<:AbstractFloat = new{T}(x)
+end
+
+Log{F<:AbstractFloat}(x::F) = LogNumber{F}(log(x))
+Log{T<:Real}(x::T) = Log(float(x))
+
+fromlog{F<:AbstractFloat}(logx::F) = LogNumber{F}(logx)
+fromlog{T<:Real}(logx::T) = fromlog(float(logx))
+
+Base.show(io::IO, x::LogNumber) = print(io, "exp(", x.log, ")")
+
+function Base.:+{F<:AbstractFloat}(x::LogNumber{F}, y::LogNumber{F})
+    LogNumber{F}(x.log + log1p(exp(y.log - x.log)))
+end
+
+export Log
 
 end
