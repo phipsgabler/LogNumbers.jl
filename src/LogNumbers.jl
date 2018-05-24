b8d3d04d581e92f376dcc55ea624d6b0b6d38bbe
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-05-24 21:44:18 +0300

    Reorder stuff a bit

--- src/LogNumbers.jl
+++ src/LogNumbers.jl
@@ -2,7 +2,7 @@ import Base
 
 module LogNumbers
 
-export LogNumber, Log, floattype,
+export LogNumber, Log, @log_str, floattype,
     LogZero, LogZero32, LogZero64,
     LogNaN, LogNaN32, LogNaN64,
     LogInf, LogInf32, LogInf64,
@@ -22,7 +22,9 @@ Base.show(io::IO, x::LogNumber{<:Union{Float32,Float64}}) = print(io, "log", '\"
 Base.show(io::IO, x::LogNumber{F}) where {F} = print(io, "Log(", F, ", ", exp(x.log), ")")
 
 
-# Constructors
+# Constructors and conversions
+include("literal_macro.jl")
+
 Log(x::F) where {F<:AbstractFloat} = LogNumber{F}(log(x))
 Log(x) = Log(float(x))
 Log(::Type{F}, x) where F = Log(convert(F, x))
@@ -44,34 +46,36 @@ Base.promote_rule(::Type{LogNumber{T}}, ::Type{S}) where {T, S<:Real} = LogNumbe
 
 # Constants
 const LogZero64 = LogNumber{Float64}(-Inf64)
-const LogZero32 = LogNumber{Float32}(-Inf32)
-const LogZero = LogZero64
-
-const LogNaN64 = LogNumber{Float64}(NaN64)
-const LogNaN32 = LogNumber{Float32}(NaN32)
-const LogNaN = LogNaN64
-
 const LogInf64 = LogNumber{Float64}(Inf64)
-const LogInf32 = LogNumber{Float32}(Inf32)
-const LogInf = LogInf64
+const LogNaN64 = LogNumber{Float64}(NaN64)
 
-Base.isinf(x::LogNumber) = isinf(x.log) && x.log > 0
-Base.isnan(x::LogNumber) = isnan(x.log)
+const LogZero32 = LogNumber{Float32}(-Inf32)
+const LogNaN32 = LogNumber{Float32}(NaN32)
+const LogInf32 = LogNumber{Float32}(Inf32)
+
+const LogZero = LogZero64
+const LogNaN = LogNaN64
+const LogInf = LogInf64
 
 Base.zero(::Type{LogNumber{Float64}}) = LogZero64
 Base.zero(::LogNumber{Float64}) = LogZero64
 Base.zero(::Type{LogNumber{Float32}}) = LogZero32
 Base.zero(::LogNumber{Float32}) = LogZero32
+
 Base.one(::Type{LogNumber{Float64}}) = LogNumber{Float64}(0e0)
 Base.one(::Type{LogNumber{Float32}}) = LogNumber{Float32}(0f0)
+
+Base.isinf(x::LogNumber) = isinf(x.log) && x.log > 0
+Base.isnan(x::LogNumber) = isnan(x.log)
 
 
 # Comparison
 Base.:(==)(x::LogNumber, y::LogNumber) = x.log == y.log
 Base.isequal(x::LogNumber, y::LogNumber) = isequal(x.log, y.log)
+Base.hash(x::LogNumber, h) = hash(x.log, hash(typeof(x), h))
 
 Base.isapprox(x::LogNumber, y::LogNumber; args...) = isapprox(x.log, y.log; args...)
-Base.eps(::Type{LogNumber{F}}) where F = Base.eps(F)
+Base.eps(::Type{LogNumber{F}}) where F = Base.eps(F) # is this the right thing? 
 
 Base.:<(x::LogNumber, y::LogNumber) = x.log < y.log
 Base.:<=(x::LogNumber, y::LogNumber) = x.log <= y.log
@@ -79,7 +83,8 @@ Base.less(x::LogNumber, y::LogNumber) = less(x.log, y.log)
 
 
 # Arithmetic
-# https://en.wikipedia.org/wiki/Log_probability
+# See https://en.wikipedia.org/wiki/Log_probability for the formulae and precautions
+
 function Base.:+{F<:AbstractFloat}(x::LogNumber{F}, y::LogNumber{F})
     y, x = minmax(x, y)
     iszero(x) && return x
@@ -97,12 +102,15 @@ Base.:*{F<:AbstractFloat}(x::LogNumber{F}, y::LogNumber{F}) = LogNumber{F}(x.log
 Base.:/{F<:AbstractFloat}(x::LogNumber{F}, y::LogNumber{F}) = LogNumber{F}(x.log - y.log)
 
 
+# Summing values in log space
+# See the following pages for explanation:
+# http://www.nowozin.net/sebastian/blog/streaming-log-sum-exp-computation.html
+# https://www.xarg.org/2016/06/the-log-sum-exp-trick-in-machine-learning/
 
 infty(::Type{T}) where {T} = one(T) / zero(T)
 infty(::T) where {T} = infty(T)
 
-# http://www.nowozin.net/sebastian/blog/streaming-log-sum-exp-computation.html
-# https://www.xarg.org/2016/06/the-log-sum-exp-trick-in-machine-learning/
+
 logsumexp(xs) = logsumexp(xs, eltype(xs))
 
 function logsumexp(xs, ::Type{LogNumber{F}}) where {F}
@@ -124,8 +132,5 @@ function expsum_update(x, acc)
         return (x, r * exp(α - x) + one(r))
     end
 end
-
-include("literal_macro.jl")
-export @log_str
 
 end
